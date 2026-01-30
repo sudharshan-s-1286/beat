7959fa0aa2fb87ddc12155fa1c61333767ac5c7f
sudharshan-s-1286 <sudharshan.s2024csbs@sece.ac.in>
2026-01-30 19:38:40 +0530

    Fixing errors, bugs and other issues - v1.0

---
+++ beat-main/server/Db/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const connectDb = () => {
+    try {
+        mongoose.connect(process.env.MONGODB_URL)
+        console.log("Database connected to:", process.env.MONGODB_URL)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export default connectDb;
